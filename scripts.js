f0bdf4269a6097bc09218d9b17391c04750c2df4
Justin Golden <justingolden21@gmail.com>
2020-08-30 20:38:05 -0700

    fixed it

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
 function factiorial(n) {
-	let result = 1;
-	for(let i = 1; i <= n; i++) {
-		result *= n;
+	let result = 1n; // big int
+	for(let i = 1n; i <= n; i++) {
+		result *= i;
 	}
 	return result;
 }
 
 function permutations(n, r) {
-	return Math.floor(factiorial(n) / factiorial(n-r) ); // floor b/c numbers get too big and decimals occur
+	return (factiorial(n) / factiorial(n-r) );
+}
+function combinations(n, r) {
+	return (factiorial(n) / factiorial(n-r) / factiorial(r) );
 }
 
 $( ()=> {
@@ -16,5 +19,11 @@ $( ()=> {
 		let r = parseInt($('#perm-r').val() );
 		let ans = permutations(n, r);
 		$('#perm-output').html( ans.toLocaleString() ); // add commas
+	});
+	$('#comb-calc').click( ()=> {
+		let n = parseInt($('#comb-n').val() );
+		let r = parseInt($('#comb-r').val() );
+		let ans = combinations(n, r);
+		$('#comb-output').html( ans.toLocaleString() ); // add commas
 	});
 });
