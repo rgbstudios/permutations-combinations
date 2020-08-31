587e6f36536426ac0412ec932162c2eb4ed8a694
Justin Golden <justingolden21@gmail.com>
2020-08-30 20:52:19 -0700

    add formulas

--- scripts.js
+++ scripts.js
@@ -20,24 +20,24 @@ function permutationsRepetition(n, list) {
 	return ans;
 }
 
+// toLocaleString to add commas
 $( ()=> {
 	$('#perm-calc').click( ()=> {
 		let n = parseInt($('#perm-n').val() );
 		let r = parseInt($('#perm-r').val() );
 		let ans = permutations(n, r);
-		$('#perm-output').html( ans.toLocaleString() ); // add commas
+		$('#perm-output').html( ans.toLocaleString() );
 	});
 	$('#comb-calc').click( ()=> {
 		let n = parseInt($('#comb-n').val() );
 		let r = parseInt($('#comb-r').val() );
 		let ans = combinations(n, r);
-		$('#comb-output').html( ans.toLocaleString() ); // add commas
+		$('#comb-output').html( ans.toLocaleString() );
 	});
 	$('#perm-rept-calc').click( ()=> {
 		let n = parseInt($('#perm-rept-n').val() );
 		let list = ($('#perm-rept-list').val().trim().split(',') ).map( x => parseInt(x) );
 		let ans = permutationsRepetition(n, list);
-		console.log(ans);
-		$('#perm-rept-output').html( ans.toLocaleString() ); // add commas
+		$('#perm-rept-output').html( ans.toLocaleString() );
 	});
 });
